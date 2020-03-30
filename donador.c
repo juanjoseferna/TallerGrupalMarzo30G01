e19ceb9737f9ebbbec7420fdc1ad7d14fd3bebec
Jhoan <jhoanuitrago@gmail.com>
2020-03-30 17:47:12 -0500

    Avanzar donador.c

--- donador.c
+++ donador.c
@@ -1,1 +1,15 @@
+#include "donador.h"
 
+void agregarDonador(donador matriz[][CAP], int lista){
+    int tipo;
+    printf("\nEscoga su tipo de sangre\n");
+    printf("1. A+\n");
+    printf("2. A-\n");
+    printf("3. B+\n");
+    printf("4. B-\n");
+    printf("5. AB+\n");
+    printf("6. AB-\n");
+    printf("7. O+\n");
+    printf("8. O-\n");
+    scanf("%d", &tipo);
+}
