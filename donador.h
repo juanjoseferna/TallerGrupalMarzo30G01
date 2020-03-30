398431fb305e545e6bcc5948c191d42945690aac
Jhoan <jhoanuitrago@gmail.com>
2020-03-30 17:12:53 -0500

    Agregar donador.h

---
+++ donador.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#define CAP 5
+
+typedef struct donador donador;
+typedef enum tipoSangre tipoSangre;
+
+struct donador{
+    char nombre[21];
+    int edad;
+};
+
+enum tipoSangre{
+    AP, AN, BP, BN, ABP, ABN, OP, ON 
+};
+
+//Funcion que permite agregar un donador
+void agregarDonador(donador matriz[][CAP], int lista);
+
+//Funcion que muestra las personas con un tipo de sangre
+void mostrarTipoSangre(donador matriz[][CAP], int lista);
+
+//funcion que muestra las personas a las que le puede donar alguien
+void mostrarDonables(donador matriz[][CAP], int lista);
