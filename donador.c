d0b19ec06d3b3fc7c58c3b6c57192231f0e9eec8
joseiguaran <60902629+joseiguaran@users.noreply.github.com>
2020-03-31 16:30:30 -0500

    Update donador.c

--- donador.c
+++ donador.c
@@ -3,9 +3,9 @@ void mostrarTipoSangre(donador matriz[][CAP], int lista[], int tipoSangre)
 {
   
     int i;
-    for (i = 0; i < lista[tipoSangre]; i++)
+    for (i = 0; i < lista[tipoSangre-1]; i++)
     {
-       donador d = matriz[tipoSangre][i];
+       donador d = matriz[tipoSangre-1][i];
         printf("Nombre del donador: %s\n Edad: %d\n", d.nombre, d.edad);
         
     }
